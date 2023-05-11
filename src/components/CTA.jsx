df2ec3b03c626858534e888338bb5e4964e22cad
Joy Brar <joybrar20001@gmail.com>
2023-05-11 09:52:54 +0530

    Finished

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import styles from '../styles'
+import Button from './Button'
 
 const CTA = () => {
   return (
-    <div>
-      
-    </div>
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} flex-col sm:flex-row bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className='flex flex-1 flex-col'>
+        <h2 className={styles.heading2}>Let's try our service now!</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Everything you need to accept card payments and grow your business anywhere on the planet</p>
+      </div>
+
+      <div className={`${styles.flexCenter} ml-0 sm:ml-10 mt-10 sm:mt-0`}>
+        <Button />
+      </div>
+    </section>
   )
 }
 
